58b64563cfcaff90e7600fc102425c4fa5ff9fe8
Yu-Chen Shu <ycshu@mail.ncku.edu.tw>
2018-03-13 13:53:07 +0800

    fix (+,-) bugs
    從三次加減法跟六次加減法的時間差來計算真正的加減法的時間

--- compute_time.c
+++ compute_time.c
@@ -4,8 +4,9 @@
 #include <time.h>
 int main()
 {
-	clock_t t1, t2;
+	clock_t t1, t2;				// variables for computing clocks 
 	double a=1.234, b=2.456;
+	double T1, T2;
 	int i, j, k, N=100000000;
 	t1 = clock();
 	for(i=0;i<N;++i)
@@ -15,8 +16,27 @@ int main()
 		a = a-b;
 	}
 	t2 = clock();
-	printf("(+,-) time:%f\n",(t2-t1)/(double) CLOCKS_PER_SEC/3.0);
+	T1 = (t2-t1)/(double) CLOCKS_PER_SEC;
+	printf("(+,-) time x 3 + 1 loop:%f\n",T1);
 	printf("(a,b)=%f %f\n", a,b);
+	t1 = clock();
+	for(i=0;i<N;++i)
+	{
+		a = a+b;
+		b = a-b;
+		a = a-b;
+		a = a+b;
+		b = a-b;
+		a = a-b;
+	}
+	t2 = clock();
+	T2 = (t2-t1)/(double) CLOCKS_PER_SEC;
+	printf("(+,-) time x 6 + 1 loop:%f\n",T2);
+	printf("Real (+,-) time: %f\n",(T2-T1)/3.0);	
+	printf("(a,b)=%f %f\n", a,b);
+
+	
+	return 0;
 	for(i=0;i<N;++i)
 	{
 		a = a*b;
@@ -46,7 +66,7 @@ int main()
 	printf("(rand) time:%f\n",(t2-t1)/(double) CLOCKS_PER_SEC);
 	printf("r=%d\n", r);
 
-	short A[10000][10000];
+	//short A[10000][10000];
 	t1 = clock();
 	for(i=0;i<N;i++)
 	{
@@ -54,7 +74,7 @@ int main()
 	}
 	t2 = clock();
 	printf("(rand) time:%f\n",(t2-t1)/(double) CLOCKS_PER_SEC);
-	printf("r=%d\n", A[9999][9999]);
+	//printf("r=%d\n", A[9999][9999]);
 
 	return;
 } 
