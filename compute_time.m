92e555ada53d2ba7c83601c807817aaede07f2e2
Yu-Chen Shu <ycshu@mail.ncku.edu.tw>
2018-03-13 14:36:46 +0800

    updated matlab file with (+,-,*,/,sin)
    改好的Matlab檔案

--- compute_time.m
+++ compute_time.m
@@ -5,20 +5,57 @@ for i=1:100000000
     b = a-b;
     b = a-b;
 end
-fprintf('(+,-) time:%f\n',toc/3);
+T1 = toc;
+%fprintf('(+,-) time:%f\n',T1);
+tic;
+for i=1:100000000
+    a = a+b;
+    b = a-b;
+    b = a-b;
+    a = a+b;
+    b = a-b;
+    b = a-b;
+end
+T2 = toc;
+%fprintf('(+,-) time:%f\n',T2);
+fprintf('(+,-) time:%f\n',(T2-T1)/3.0);
+
 tic;
 for i=1:100000000
     a = a*b;
     b = a/b;
     b = a/b;
 end
-fprintf('(*,/) time:%f\n',toc/3);
+T1 = toc;
+%fprintf('(*,/) time:%f\n',T1);
+tic;
+for i=1:100000000
+    a = a*b;
+    b = a/b;
+    b = a/b;
+    a = a*b;
+    b = a/b;
+    b = a/b;
+
+end
+T2 = toc;
+%fprintf('(*,/) time:%f\n',T2);
+fprintf('(*,/) time:%f\n',(T2-T1)/3.0);
+tic;
+for i=1:100000000
+    a = sin(a);
+end
+T1 = toc;
+fprintf('(sin) time x 1:%f\n',T1);
 tic;
 for i=1:100000000
     a = sin(b);
     b = sin(a);
 end
-fprintf('(sin) time:%f\n',toc/2);
+T2 = toc;
+fprintf('(sin) time x 2:%f\n',T2);
+fprintf('(sin) time:%f\n', T2-T1);
+return;
 tic;
 for i=1:100000000
     a = randi(32768,1);
