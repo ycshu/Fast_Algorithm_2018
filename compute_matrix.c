885ba693afb45205cb2129f4a19352cea33434f0
ycshu <ycshu@mail.ncku.edu.tw>
2018-04-09 22:54:27 +0800

    compute_matrix_old
    compute_matrix_old

--- compute_matrix.c
+++ compute_matrix.c
@@ -5,7 +5,7 @@
 int main()
 {
 	clock_t t1, t2;				// variables for computing clocks 
-	double **A, *x, *b, T1;
+	double **A, *x, *b, T1, s;
 	int i, j, N=10000;
 
 	srand(time(NULL));
@@ -14,30 +14,51 @@ int main()
 	{
 		A = (double **) malloc( N * sizeof(double*) );
 		A[0] = (double *) malloc( N*N*sizeof(double));
-		for(i=1;i<N;++i) A[i] = A[i-1] + N;
+		#pragma omp parallel for 
+		for(i=1;i<N;++i) A[i] = A[0] + i*N;
 		x = (double *) malloc( N * sizeof(double) );
 		b = (double *) malloc( N * sizeof(double) );
 		
+		#pragma omp parallel for
 		for(i=0;i<N;++i)
 		{
+			x[i] = 1.0*rand()/RAND_MAX;
+			#pragma omp parallel for
 			for(j=0;j<N;++j)
 			{
-				A[i][j] = rand();
+				A[i][j] = 1.0*rand()/RAND_MAX;
 			}
-			x[i] = rand();
 		}
+
 		t1 = clock();
 		for(i=0;i<N;++i) 
 		{
-			b[i] = 0.0;
+			s = 0.0;
 			for(j=0;j<N;++j)
 			{
-				b[i] += A[i][j]*x[j];
+				s += A[i][j]*x[j];
 			}
+			b[i] = s;
 		}
 		t2 = clock();
 		T1 = (t2-t1)/(double) CLOCKS_PER_SEC;
-		printf("Matrix time vector :%f\n",T1);
+		printf("Matrix time vector (not parallel) N=%d : %f\n",N, T1);
+		
+		t1 = clock();
+		#pragma omp parallel for private(i,s) num_threads(1)
+		for(i=0;i<N;++i) 
+		{
+			s = 0.0;
+			#pragma omp parallel for reduction(+: s)
+			for(j=0;j<N;++j)
+			{
+				s += A[i][j]*x[j];
+			}
+			b[i] = s;
+		}
+		t2 = clock();
+		T1 = (t2-t1)/(double) CLOCKS_PER_SEC;
+		printf("Matrix time vector for N=%d : %f\n",N, T1);
 		free(b);
 		free(x);
 		free(A[0]);
